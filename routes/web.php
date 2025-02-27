b2e980c579a5b6479c5d3d79682adcac015d7ffb
sondrefivelsdal <sondrebfivelsdal@gmail.com>
2025-02-27 10:56:49 +0100

    guest and auth middleware i route.

--- routes/web.php
+++ routes/web.php
@@ -8,20 +8,28 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Routes for the Login and Register pages.
-Route::get('/register', [AuthController::class, 'showRegister'])->name('show.register');
-Route::get('/login', [AuthController::class, 'showLogin'])->name('show.login');
-Route::Post('/register', [AuthController::class, 'register'])->name('register');
-Route::Post('/login', [AuthController::class, 'login'])->name('login');
+
+// Route for the logout function.
 Route::Post('/logout', [AuthController::class, 'logout'])->name('logout');
 
 
-// Routes for the NinjaController. 
-Route::get('/sondre', [NinjaController::class, 'index'])->name('ninjas.index');
-Route::get('/sondre/create', [NinjaController::class, 'create'])->name('ninjas.create');
-Route::get('/sondre/{ninja}', [NinjaController::class, 'show'])->name('ninjas.show');
-Route::post('/sondre', [NinjaController::class, 'store'])->name('ninjas.store');
-Route::delete('/sondre/{ninja}', [NinjaController::class, 'destroy'])->name('ninjas.destroy');
+// Routes for the Login and Register pages.
+Route::middleware('guest')->controller()->group(function() {
+    Route::get('/register', 'showRegister')->name('show.register');
+    Route::get('/login', 'showLogin')->name('show.login');
+    Route::Post('/register', 'register')->name('register');
+    Route::Post('/login', 'login')->name('login');
+});
+
+// Routes for the NinjaController. with auth middleware.
+Route::middleware('auth')->controller(NinjaController::class)->group(function() {
+    Route::get('/sondre', 'index')->name('ninjas.index');
+    Route::get('/sondre/create', 'create')->name('ninjas.create');
+    Route::get('/sondre/{ninja}', 'show')->name('ninjas.show');
+    Route::post('/sondre', 'store')->name('ninjas.store');
+    Route::delete('/sondre/{ninja}', 'destroy')->name('ninjas.destroy');
+});
+
 
 
 
